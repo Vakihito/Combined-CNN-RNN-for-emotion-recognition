6ccba39decb6c0fa30217e17d61250f9e75e9f7b
Victor Akihito Kamada Tomita <40372914+Vakihito@users.noreply.github.com>
2021-04-26 20:33:28 -0300

    Add files via upload

--- src/config.py
+++ src/config.py
@@ -12,12 +12,12 @@ elif env == 'local':
     RAW_VIDEO_DATA_DIR = "/Users/denisrangulov/Fake Colab/aff_wild2_videos"
     PROCESSED_FRAMES_DATA_DIR = "/Users/denisrangulov/Fake Colab/cropped_aligned"
     PROCESSED_SEQUENCES_DATA_DIR = "/Users/denisrangulov/Fake Colab/sequences"
-    PREDICTIONS = '/Users/denisrangulov/predictions/submission-1'
+    PREDICTIONS = '/content/Combined-CNN-RNN-for-emotion-recognition/predictions'
     INTERPOLATION_PREDICTIONS = '/Users/denisrangulov/predictions/interpolated/submission-1'
     MODEL_CHECKPOINTS_DIR = "/Users/denisrangulov/Fake Colab/model_checkpoints"
-    SAVED_CNN_EXTRACTOR_MODEL = '/Users/denisrangulov/Google Drive/EmotionRecognition/data/checkpoints/' \
+    SAVED_CNN_EXTRACTOR_MODEL = '/content/Combined-CNN-RNN-for-emotion-recognition/data/checkpoints/' \
                                 'simple_cnn_ccc_loss.0001-0.7792.hdf5'
-    SAVED_RNN_MODEL = '/Users/denisrangulov/Google Drive/EmotionRecognition/data/checkpoints/' \
+    SAVED_RNN_MODEL = '/content/Combined-CNN-RNN-for-emotion-recognition/data/checkpoints/' \
                       'gru_ccc_loss-epoch:17-loss:0.6913-ccc_v:0.2252-ccc_a:0.3922.hdf5'
 
 elif env == 'vm':
@@ -27,9 +27,9 @@ elif env == 'vm':
     PROCESSED_SEQUENCES_DATA_DIR = "/home/rangulov/sequences_simple_cnn_ccc_loss.0001-0.7792"
     PREDICTIONS = '/home/rangulov/predictions/gru_ccc_loss-epoch:17-loss:0.6913-ccc_v:0.2252-ccc_a:0.3922.hdf5'
     MODEL_CHECKPOINTS_DIR = "/home/rangulov/EmotionRecognition/data/checkpoints"
-    SAVED_CNN_EXTRACTOR_MODEL = '/home/rangulov/EmotionRecognition/data/checkpoints/' \
+    SAVED_CNN_EXTRACTOR_MODEL = '/content/Combined-CNN-RNN-for-emotion-recognition/data/checkpoints/' \
                                 'simple_cnn_ccc_loss.0001-0.7792.hdf5'
-    SAVED_RNN_MODEL = '/home/rangulov/EmotionRecognition/data/checkpoints/' \
+    SAVED_RNN_MODEL = '/content/Combined-CNN-RNN-for-emotion-recognition/data/checkpoints/' \
                       'gru_ccc_loss-epoch:17-loss:0.6913-ccc_v:0.2252-ccc_a:0.3922.hdf5'
 
 RANDOM_STATE = 42
